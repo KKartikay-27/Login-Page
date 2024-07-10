b24cd5a3a72ea1c4d61928a2ef85abf39ea71ac7
kumarkartikay <kumarkartikay005@gmail.com>
2024-07-10 11:47:05 +0530

    completed login backend

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,15 +1,60 @@
 const express = require("express");
 const User = require("../models/userModel");
+const bcrypt = require('bcrypt');
 
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
+    try {
 
+        const userExists = await User.findOne({ email: req.body.email });
+
+        if (userExists) {
+            res.send({
+                "success": false,
+                "message": "User already exists"
+            })
+        }
+
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        req.body.password = hashedPassword;
+
+
+        const newUser = new User(req.body);
+        await newUser.save();
+        return res.json("User created")
+
+    } catch (error) {
+        res.json(error);
+    }
 
 });
 
 router.post("/login", async (req, res) => {
-  
+    const user = await User.findOne({email : req.body.email});
+    if(!user){
+        res.send({
+            success : false,
+            message : "User does not exist, please register"
+        })
+    }
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+
+    if(!validPassword){
+        res.send({
+            success : false,
+            message : "Wrong Password"
+        })
+    }
+
+    res.send({
+        success : true,
+        message : "User Logged in"
+    })
+
 });
 
 
